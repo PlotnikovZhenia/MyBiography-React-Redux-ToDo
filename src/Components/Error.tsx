b97b0ced1576d3d7194d6e44b595c377744d43f9
PlotnikovZhenia <niaplotzhekovmi@gmail.com>
2021-12-10 01:29:35 +0200

    my useful message

--- src/Components/Error.tsx
+++ src/Components/Error.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import classes from './Error.module.scss';
+import Navbar from "./Navbar";
 
 const Error=()=>{
     return(
         <div className={classes.error_information}>
+                <Navbar/>
                 <h1>404 Not Found</h1>
                 <p>Указаный адрес пока что не существует</p>
                 <Link to="/">Назад к главной</Link>
